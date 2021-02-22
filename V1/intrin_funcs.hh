157fa16347e1c8fd6589fa63dfc8a0c747f1a0d0
AVasK <avask17@gmail.com>
2021-02-22 12:25:28 +0300

    added Clang&GCC support for arch attributes

--- V1/intrin_funcs.hh
+++ V1/intrin_funcs.hh
@@ -1,6 +1,16 @@
 ///!!! DO NOT COMPILE AS AN INDEPENDENT FILE
 #pragma once
 
+#ifdef ARCH_x86
+    #if C_CLANG
+        #pragma clang attribute push (__attribute__((target("sse, sse2,sse4.1,sse4.2,ssse3,avx,avx2"))), apply_to=function)
+    #elif C_GCC
+        #pragma GCC push_options
+        #pragma GCC target("sse4.2", "ssse3", "avx", "avx2")
+    #endif
+#endif
+
+
 static inline auto iload_128(void const * p) -> __m128i
 {
     // load aligned integer vector from array p
@@ -35,10 +45,7 @@ func i16_add_avx (
 -> Array<i16>
 {
     auto n_regs = a1.size_in_registers();
-    auto align = a1.get_alignment();
-    
-    //Array<i16> res (a1.size());
-    for (size_t i=0; i < n_regs; i++)
+    for (size_t i=0; i < n_regs; ++i)
     {
         //FIXME: Need separate function to get register start address.
         // load #elems_per_reg consecutive elements from a1:
@@ -62,10 +69,7 @@ func i16_add_sse (
 -> Array<i16>
 {
     auto n_regs = a1.size_in_registers();
-    auto align = a1.get_alignment();
-    
-    //Array<i16> res (a1.size());
-    for (size_t i=0; i < n_regs; i++)
+    for (size_t i=0; i < n_regs; ++i)
     {
         // load <n_per_reg> consecutive elements from a1:
         auto _a1 = iload_128(&a1[i<<3]);
@@ -76,3 +80,13 @@ func i16_add_sse (
     }
     return res;
 }
+
+
+
+#ifdef ARCH_x86
+    #if C_CLANG
+        #pragma clang attribute pop
+    #elif C_GCC
+        #pragma GCC pop_options
+    #endif
+#endif
